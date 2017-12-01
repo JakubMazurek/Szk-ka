5e28d3b6433d83958d9bc69bba83826a5d3608ba
Mazurek <32750392+JakubMazurek@users.noreply.github.com>
2017-12-01 01:08:25 +0100

    Add files via upload

---
+++ Zad_Dom 01_12_17/Zad 1 na 01_12_2017.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+ int liczba,system,reszta,krok=1,i=1;
+ string bin="";
+
+ cout<<"Podaj liczbe oraz podaj system liczbowy"<<endl;
+
+ cin>>liczba>>system;
+
+ if (liczba!=0){
+    if(system!=0){
+        switch(system){
+    case 10:
+         cout<<"\n dziesietny = "<<liczba;
+         break;
+    case 8:
+          cout<<"\n osemkowy = "<<oct<<liczba;
+          break;
+    case 16:
+         cout<<"\n szesnastkowy = "<<hex<<liczba;
+    case 2:
+        while(liczba>0)
+        {
+         if (liczba%2==0){
+            bin="0"+bin;
+         }
+         else {
+            bin="1"+bin;
+         };
+         liczba=liczba/2;
+        }
+        cout<< bin;
+        break;
+    // case 7 podda³em siê :c
+
+
+
+
+
+
+        }
+        }
+    }
+    return 0;
+ }
+
